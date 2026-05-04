296a75e3c207b31e27d2f04454cd1a4049575469
alkhnanyaslam-sys <alkhnanyaslam@gmail.com>
2026-05-04 09:44:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls, idle
 from pytgcalls.types import MediaStream
@@ -8,20 +7,24 @@ API_HASH = "e182f84704911e253c33b8015b922cdd"
 CHAT_ID = -1003373397195
 MAKKAH_STREAM = "https://backup.qurango.net/radio/mishary_rashid"
 
-app = Client("quran_session", api_id=API_ID, api_hash=API_HASH)
-call_py = PyTgCalls(app)
+client = Client(
+    "quran_session",
+    api_id=API_ID,
+    api_hash=API_HASH
+)
+app = PyTgCalls(client)
 
-@app.on_message(filters.command("start_quran") & filters.chat(CHAT_ID))
-async def start_quran(client, message):
+@client.on_message(filters.command("start_quran") & filters.chat(CHAT_ID))
+async def start_quran(c, message):
     await message.reply("🕌 جاري تشغيل بث الحرم المكي...")
-    call_py.play(CHAT_ID, MediaStream(MAKKAH_STREAM))
+    app.play(CHAT_ID, MediaStream(MAKKAH_STREAM))
     await message.reply("✅ تلاوة الحرم المكي تُبث الآن 🎙️")
 
-@app.on_message(filters.command("stop_quran") & filters.chat(CHAT_ID))
-async def stop_quran(client, message):
-    call_py.leave_group_call(CHAT_ID)
+@client.on_message(filters.command("stop_quran") & filters.chat(CHAT_ID))
+async def stop_quran(c, message):
+    app.leave_group_call(CHAT_ID)
     await message.reply("⏹️ تم إيقاف البث")
 
+client.start()
 app.start()
-call_py.start()
 idle()
