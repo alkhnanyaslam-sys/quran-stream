14857bee10f45ed1484c92862f1551ad5fe2f920
alkhnanyaslam-sys <alkhnanyaslam@gmail.com>
2026-05-04 08:53:31 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import asyncio
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+from pytgcalls.types import Update
+from pytgcalls.types.input_stream import InputAudioStream
+from pytgcalls.types.input_stream.audio import AudioPiped
+
+API_ID = 38091412
+API_HASH = "e182f84704911e253c33b8015b922cdd"
+CHAT_ID = -1003373397195
+MAKKAH_STREAM = "https://backup.qurango.net/radio/mishary_rashid"
+
+app = Client("quran_session", api_id=API_ID, api_hash=API_HASH)
+call_py = PyTgCalls(app)
+
+@app.on_message(filters.command("start_quran") & filters.chat(CHAT_ID))
+async def start_quran(client, message):
+    await message.reply("🕌 جاري تشغيل بث الحرم المكي...")
+    await call_py.join_group_call(
+        CHAT_ID,
+        InputAudioStream(AudioPiped(MAKKAH_STREAM)),
+    )
+    await message.reply("✅ تلاوة الحرم المكي تُبث الآن 🎙️")
+
+@app.on_message(filters.command("stop_quran") & filters.chat(CHAT_ID))
+async def stop_quran(client, message):
+    await call_py.leave_group_call(CHAT_ID)
+    await message.reply("⏹️ تم إيقاف البث")
+
+async def main():
+    await call_py.start()
+    await app.start()
+    print("✅ شغال")
+    await asyncio.get_event_loop().run_forever()
+
+asyncio.run(main())
